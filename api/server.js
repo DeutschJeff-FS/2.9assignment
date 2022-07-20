f31b09141952b4caaf53fa4d8a4f053f7547f58e
Jeff Deutsch <jadeutsch@student.fullsail.edu>
2022-07-20 01:24:20 -0400

    basic CRA build

--- api/server.js
+++ api/server.js
@@ -20,17 +20,17 @@ db.on("error", (error) => console.error(error));
 db.once("open", () => console.log(`Database Connection Established.`));
 
 app.use(express.json());
-app.use("/movies", movieRouter);
+app.use("/api/v1/movies", movieRouter);
 
 // Looks into React build folder for static build
-//app.use(express.static(path.join(__dirname, "../reactjs/build")));
+app.use(express.static(path.join(__dirname, "../reactjs/build")));
 
 /**
  * For any routes not defined by API, assume it's a direct request to a clientSide route
  */
-// app.get("/*", (req, res, next) => {
-//   res.sendFile(path.join(__dirname, "../reactjs/build", "index.html"));
-// });
+app.get("/*", (req, res, next) => {
+  res.sendFile(path.join(__dirname, "../reactjs/build", "index.html"));
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on ${PORT}.`);
